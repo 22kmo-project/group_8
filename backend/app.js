e31a0cc1f5177798cd7bebddf233ac4f298b3848
Antti Hupli <t1huan00@students.oamk.fi>
2022-11-17 03:14:17 +0200

    jos taa korvais ton edellisen

--- backend/app.js
+++ backend/app.js
@@ -6,12 +6,6 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
-var userRouter = require('./routes/user');
-
-var loginRouter = require('./routes/login');
-
-
-
 var app = express();
 
 app.use(logger('dev'));
@@ -22,10 +16,5 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-
-app.use('/user', userRouter);
-
-app.use('/login', loginRouter);
-
 
 module.exports = app;
