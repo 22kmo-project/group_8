6426ad8c86046fb9ab0a581b85fe44c637e5f993
Jonna Huotari <t1hujo04@students.oamk.fi>
2022-11-17 18:24:57 +0200

    account taulun crud luotu ja toimii

---
+++ backend/models/account_model.js
@@ -0,0 +1,28 @@
+const db = require('../database');
+
+const account = {
+  getById: function(id, callback) {
+    return db.query('select * from account where id_account=?', [id], callback);
+  },
+  getAll: function(callback) {
+    return db.query('select * from account', callback);
+  },
+  add: function(add_data, callback) {
+    return db.query(
+      'insert into account (id_account,account_type,balance) values(?,?,?)',
+      [add_data.id_account, add_data.account_type, add_data.balance],
+      callback
+    );
+  },
+  delete: function(id, callback) {
+    return db.query('delete from account where id_account=?', [id], callback);
+  },
+  update: function(id, update_data, callback) {
+    return db.query(
+      'update account set account_type=?,balance=? where id_account=?',
+      [update_data.account_type, update_data.balance, id],
+      callback
+    );
+  }
+};
+module.exports = account;
